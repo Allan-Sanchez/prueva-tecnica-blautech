3c7f0d505d49dac23e5065de648bceab6671a06e
Allan-Sanchez <asanchezrixtun@gmail.com>
2022-02-07 18:45:25 -0600

    init proyect with react with vite, add tailwindcss

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./index.css";
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <div className="">
+      <h1 className="font-bold text-2xl text-blue-900 test">test 1</h1>
+    </div>
+  );
+}
+
+export default App;
